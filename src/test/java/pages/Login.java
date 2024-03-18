f00e4dfc0a4c0c5df05187b1a8dcca5ab1c27ecd
Titamita <juliadancs@gmail.com>
2024-03-18 15:58:10 -0400

    Login File with Env. Variables Example

---
+++ src/test/java/pages/Login.java
@@ -0,0 +1,16 @@
+package pages;
+
+public class Login {
+
+    public static void main(String[] args) {
+
+        String username = System.getenv("username");
+        String password = System.getenv("password");
+
+        System.out.println(username);
+        System.out.println(password);
+
+
+
+    }
+}
